12e038af98e7bd010d67bae037fdeb11ad74fd59
Brad Cupp <bradjcupp@gmail.com>
2018-05-26 21:20:45 -0400

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$( document ).ready(function() {
+function sliderChange(value){
+  if (value > 15) {
+    $("#textDropDown1").slideDown("slow");
+  } else {
+    $("#textDropDown1").slideUp("slow");
+  }
 
-});
+  if (value > 50) {
+    $("#textDropDown2").slideDown("slow");
+  } else {
+    $("#textDropDown2").slideUp("slow");
+  }
+
+  if (value > 75) {
+    $("#textDropDown3").slideDown("slow");
+  } else {
+    $("#textDropDown3").slideUp("slow");
+  }
+
+}
